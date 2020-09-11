86b28345777fab1bc6f380e3845c8f762b8085dc
mpolaina <epetekandemor@gmail.com>
2020-09-12 01:26:46 +0200

    ok

--- MERNtasks/servidor/controllers/usuarioController.js
+++ MERNtasks/servidor/controllers/usuarioController.js
@@ -1,6 +1,15 @@
 const Usuario = require('../models/Usuario')
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator')
+
 exports.crearUsuario = async (req, res) => {
     
+    // Revisar si hay errores
+    const errores = validationResult(req)
+    if( !errores.isEmpty() ){
+        return res.status(400).json({ errores: errores.array() })
+    }
+  
     // Extraer email y password
     const { email, password } = req.body;
     
@@ -10,9 +19,15 @@ exports.crearUsuario = async (req, res) => {
         
         if(usuario) {
           return res.status(400).json({ msg: 'El usuario ya existe' })
-        }
+        }        
+        
         // crea nuevo usuario
         usuario = new Usuario(req.body)
+        
+        // Hashear el password
+        const salt = await bcrypt.genSalt(10)
+        usuario.password = await bcrypt.hash(password, salt)
+        
         //guardar usuario
         await usuario.save()
         
